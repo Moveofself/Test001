2ed258b33f345ca380fac1e55f35ed522c1f1ed2
Moveofself <40795934+Moveofself@users.noreply.github.com>
2020-02-11 15:02:50 +0800

    Modify namespace

--- TestCode/WindowsFormsApp1/Form1.Designer.cs
+++ TestCode/WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace TestForm
 {
     partial class Form1
     {
--- TestCode/WindowsFormsApp1/Form1.cs
+++ TestCode/WindowsFormsApp1/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
 
-namespace WindowsFormsApp1
+namespace TestForm
 {
     public partial class Form1 : Form
     {
